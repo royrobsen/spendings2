94e4475b21bbf36fb72c6f5e43976958efa122f6
royrobsen <rbrannath@yahoo.de>
2016-01-15 12:38:40 +0100

    Update app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('31.16.201.53', '84.46.57.47','127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('31.16.201.53','213.54.5.69', '84.46.57.47','127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
